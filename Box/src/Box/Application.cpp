92ccf2abd60b44b87cd125bd799d412d6da55a6f
falcondev <tinhtcp@gmail.com>
2024-10-28 20:00:18 -0600

    Some minor adjustments

--- Box/src/Box/Application.cpp
+++ Box/src/Box/Application.cpp
@@ -1,14 +1,12 @@
 #include "boxpch.h"
 #include "Application.h"
 
+#include "Box/Core.h"
 #include "Box/Log.h"
 
 #include <glad/glad.h>
 
 namespace Box {
-
-	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
-
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
@@ -17,7 +15,7 @@ namespace Box {
 		s_Instance = this;
 
 		m_Window = std::unique_ptr<Window>(Window::Create());
-		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+		m_Window->SetEventCallback(BOX_BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
@@ -40,7 +38,7 @@ namespace Box {
 	void Application::OnEvent(Event& e) 
 	{
 		EventDispatcher dispatcher(e);
-		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowCloseEvent>(BOX_BIND_EVENT_FN(Application::OnWindowClose));
 
 		BOX_CORE_TRACE("{0}", e.ToString());
 
